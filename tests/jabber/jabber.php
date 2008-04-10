99b65689c1c285b2f2a63aafd93d08fea394d0a8
geoff <geoff@4e41a753-b37d-4768-8075-ae94130ddde9>
2008-04-10 01:35:57 +0000

    Quick Jabber client test using xmpphp
    git-svn-id: svn+ssh://svn.foohack.com/svn/twitterpg/trunk@3 4e41a753-b37d-4768-8075-ae94130ddde9

---
+++ tests/jabber/jabber.php
@@ -0,0 +1,59 @@
+#!/usr/bin/php
+<?php
+/**
+ * Jabber testing script
+ *
+ * Execute this script from a terminal window
+ * Send a Jabber IM to tpg_tester@jabber.org
+ * Profit!
+ * 
+ * Send "quit" to end the script
+ */
+
+include("xmpphp/xmpp.php");
+
+// test account information
+$host = 'jabber.org';
+$port = 5222;
+$username = 'tpg_tester';
+$password = 'tpgrox';
+$resource = 'xmpphp';
+$server = null;
+
+$xmpp = new XMPP($host, $port, $username, $password, $resource, $server, true, LOGGING_INFO);
+$xmpp->connect();
+
+while (!$xmpp->disconnected) {
+	$payloads = $xmpp->processUntil(array('message', 'presence', 'end_stream', 'session_start'));
+	foreach ($payloads as $event) {
+		$pl = $event[1];
+		switch($event[0]) {
+			case 'message':
+				echo "---------------------------------------------------------------------------------\n";
+				echo "Message from: {$pl['from']}\n";
+				if ($pl['subject']) {
+					echo "Subject: {$pl['subject']}\n";
+				} 
+				echo $pl['body'] . "\n";
+				echo "---------------------------------------------------------------------------------\n";
+				$xmpp->message($pl['from'], "Thanks for sending me \"{$pl['body']}\".", $pl['type']);
+				if ($pl['body'] == 'quit') {
+					$xmpp->disconnect();
+				}
+				if ($pl['body'] == 'break') {
+					$xmpp->send("</end>");
+				}
+				break;
+			case 'presence':
+				echo "Presence: {$pl['from']} [{$pl['show']}] {$pl['status']}\n";
+				break;
+			case 'session_start':
+				$xmpp->presence('Ready to roxorz!');
+				break;
+		}
+	}
+}
+
+unset($xmpp);
+
+?>
